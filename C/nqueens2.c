1cd6130a8a8d429f60ae80fd3a9ae2d7117a6965
gqtc <xorp2008@163.com>
2015-08-21 15:18:25 +0800

    edit file

--- C/nqueens2.c
+++ C/nqueens2.c
@@ -1,26 +1,8 @@
 /*****************************************************
-The n-queens puzzle is the problem of placing n queens on an n*n 
-chessboard such that no two queens attack each other.
+Follow up for N-Queens problem.
 
-Given an integer n, return all distinct solutions to the n-queens puzzle.
-
-Each solution contains a distinct board configuration of the n-queens' 
-placement, where 'Q' and '.' both indicate a queen and an empty space respectively.
-
-For example,
-There exist two distinct solutions to the 4-queens puzzle:
-
-[
- [".Q..",  // Solution 1
-  "...Q",
-  "Q...",
-  "..Q."],
-
- ["..Q.",  // Solution 2
-  "Q...",
-  "...Q",
-  ".Q.."]
-]
+Now, instead outputting board configurations, return the total 
+number of distinct solutions.
 
 *******************************************************/
 #include <stdio.h>
